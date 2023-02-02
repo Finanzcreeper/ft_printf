010b364604c202c1d1079761b7060cdc1647dde7
Nico Reher <nreher@student.42berlin.de>
2023-02-02 18:06:07 +0100

    test main updated

--- .main.c
+++ .main.c
@@ -6,7 +6,7 @@
 /*   By: nreher <nreher@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 18:59:21 by nreher            #+#    #+#             */
-/*   Updated: 2023/02/02 17:22:35 by nreher           ###   ########.fr       */
+/*   Updated: 2023/02/02 18:04:19 by nreher           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,20 +30,29 @@ int main(void)
 	d = 1234567;
 	u = 4294967295;
 	x = 380358127;
-	a = ft_printf("testing:\nchar		't'		:	'%c'\nstring		'test'		:	'%s'\npointer of char	't'		:	'%p'\nnullpointer:	(nil)		:	'%p'\nint		'1234567'	:	'%d'\nint		'1234567'	:	'%i'\nunsigned int	'4294967295'	:	'%u'\nhex of		'380358127'	:	'%x'\nuppercase hex of'380358127'	:	'%X'\n'%%'\n", c, s, &c, (void *)0, d, d, u, x, x);
-		b = printf("testing:\nchar		't'		:	'%c'\nstring		'test'		:	'%s'\npointer of char	't'		:	'%p'\nnullpointer:	(nil)		:	'%p'\nint		'1234567'	:	'%d'\nint		'1234567'	:	'%i'\nunsigned int	'4294967295'	:	'%u'\nhex of		'380358127'	:	'%x'\nuppercase hex of'380358127'	:	'%X'\n'%%'\n", c, s, &c, (void *)0, d, d, u, x, x);
+	printf("testing mine:\n");
+	a = ft_printf("char		't'		:	'%c'\nstring		'test'		:	'%s'\npointer of char	't'		:	'%p'\nnullpointer:	(nil)		:	'%p'\nint		'1234567'	:	'%d'\nint		'1234567'	:	'%i'\nunsigned int	'4294967295'	:	'%u'\nhex of		'380358127'	:	'%x'\nuppercase hex of'380358127'	:	'%X'\n'%%'\n", c, s, &c, (void *)0, d, d, u, x, x);
+	printf("testing original:\n");
+		b = printf("char		't'		:	'%c'\nstring		'test'		:	'%s'\npointer of char	't'		:	'%p'\nnullpointer:	(nil)		:	'%p'\nint		'1234567'	:	'%d'\nint		'1234567'	:	'%i'\nunsigned int	'4294967295'	:	'%u'\nhex of		'380358127'	:	'%x'\nuppercase hex of'380358127'	:	'%X'\n'%%'\n", c, s, &c, (void *)0, d, d, u, x, x);
 	printf("mine: %d=%d :original\n-----------------------------------------\n", a, b);
 
-	a = ft_printf("%x%x\n",0,0);
-	b = printf("%x%x\n",0,0);
+	a = ft_printf("consecutive hex (0,0): %x%x\n",0,0);
+	b = printf("consecutive hex (0,0): %x%x\n",0,0);
 	printf("mine: %d=%d :original\n-----------------------------------------\n", a, b);
 
+	ft_printf("invalid inputs:\n");
 	a = ft_printf(0);
 	b = printf(0);
 	printf("mine: %d=%d :original\n-----------------------------------------\n", a, b);
 
-	a = ft_printf("%p\n", (void *)0);
-	b = printf("%p\n", (void *)0);
+	a = ft_printf("nullpointer: %p\n", (void *)0);
+	b = printf("nullpointer: %p\n", (void *)0);
+	printf("mine: %d=%d :original\n-----------------------------------------\n", a, b);
+	a = ft_printf("pointer -1: %p\n", (void *)-1);
+	b = printf("pointer -1: %p\n", (void *)-1);
+	printf("mine: %d=%d :original\n-----------------------------------------\n", a, b);
+	a = ft_printf("hex of -1: %x\n", -1);
+	b = printf("hex of -1: %x\n", -1);
 	printf("mine: %d=%d :original\n-----------------------------------------\n", a, b);
 	return(0);
 }
